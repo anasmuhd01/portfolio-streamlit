9a3da880d50dfa3511527b707310fc57ccad8885
Anas Muhammed <ansmuhd01@gmail.com>
2023-08-11 23:13:55 +0530

    main page completed

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas
 
 st.set_page_config(layout="wide")
 
@@ -22,3 +23,18 @@ tab1.write("below you can find some of the apps i have built in python")
 
 content2 = "below you can find some of the apps i have built in python "
 st.write(content2)
+
+df = pandas.read_csv("data.csv",sep=";")
+col3, col4 = st.columns(2)
+
+with col3:
+    for index, row in df[:10].iterrows():
+        st.title(row["title"])
+        st.write(row["description"])
+        st.image("images(1)/"+ row["image"])
+        st.write(f"[soure code]({row['url']})")
+with col4:
+    for index, row in df[10:].iterrows():
+        st.title(row["title"])
+        st.write(row["description"])
+        st.image("images(1)/" + row["image"])
