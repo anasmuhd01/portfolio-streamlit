7d6e3e1c46ef04251ce0b795c08970abc7e79c92
Anas Muhammed <ansmuhd01@gmail.com>
2023-08-12 16:58:13 +0530

    instead of dotenv .py file used - testing vars-pas.ph

--- send_email.py
+++ send_email.py
@@ -1,21 +1,17 @@
 import smtplib
 import ssl
-
-from dotenv import load_dotenv
+from vars import pas
 import os
-load_dotenv()
 
 
 def send_email(message):
     host = "smtp.gmail.com"
     port = 465
     username = "beerankutty1509@gmail.com"
-    pasword = os.getenv("PASSWORD")
+    pasword = pas.PASSWORD
 
     context = ssl.create_default_context()
     receiver = "beerankutty1509@gmail.com"
     with smtplib.SMTP_SSL(host, port, context=context) as server:
         server.login(username, pasword)
         server.sendmail(username, receiver, message)
-
-
