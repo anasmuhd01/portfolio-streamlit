1b6cff694405b36e43de7ae6c779855b7f357725
Anas Muhammed <ansmuhd01@gmail.com>
2023-08-12 17:14:40 +0530

    secured pasword and ids using streamlit secrets

--- send_email.py
+++ send_email.py
@@ -6,12 +6,12 @@ import ssl
 def send_email(message):
     host = "smtp.gmail.com"
     port = 465
-    username = "beerankutty1509@gmail.com"
+    username = st.secrets["mail_id"]
     pasword = st.secrets["g_pas"]
 
 
     context = ssl.create_default_context()
-    receiver = "beerankutty1509@gmail.com"
+    receiver = st.secrets["mail_id"]
     with smtplib.SMTP_SSL(host, port, context=context) as server:
         server.login(username, pasword)
         server.sendmail(username, receiver, message)
