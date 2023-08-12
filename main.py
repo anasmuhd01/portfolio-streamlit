481f740f7a98cc52d0a069bf5e2f201c66cb11b2
Anas Muhammed <ansmuhd01@gmail.com>
2023-08-12 15:59:52 +0530

    photo change and link changed

--- main.py
+++ main.py
@@ -2,29 +2,29 @@ import streamlit as st
 import pandas
 
 st.set_page_config(layout="wide")
-
+df = pandas.read_csv("data.csv",sep=";")
 col1, col2 = st.columns(2)
 
 with col1:
 
-    st.image("images(1)/photo.png")
+    st.image("images(1)/photo.jpg",width=500)
 
 content = """ 
-this is a  sample content"""
+As a recent graduate with a strong passion for programming and software development, 
+I am excited to embark on a career as a Python developer. 
+I possess a solid foundation in Python programming and 
+a keen desire to learn and contribute in a dynamic team environment. 
+My academic background has equipped me with problem-solving skills, an analytical mindset, 
+and a determination to excel in the field of software development.
+"""
 
 with col2:
     st.title("Anas Muhammed")
     st.info(content)
 
-tab1 = st.container()
-
-
-tab1.write("below you can find some of the apps i have built in python")
-
 content2 = "below you can find some of the apps i have built in python "
-st.write(content2)
+st.subheader(content2)
 
-df = pandas.read_csv("data.csv",sep=";")
 col3, col4 = st.columns(2)
 
 with col3:
