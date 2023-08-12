26f827f862369a40d726966e3dd2ab0b8b8ee097
Anas Muhammed <ansmuhd01@gmail.com>
2023-08-12 14:39:09 +0530

    main page completed

---
+++ pages/contact_us.py
@@ -0,0 +1,18 @@
+import streamlit as st
+from send_email import send_email
+st.header("contact us")
+
+with st.form(key="my_form"):
+    user_email = st.text_input("enter your email:")
+    raw_message = st.text_area("enter your message:")
+    message = f"""\
+Subject: New message from {user_email}
+
+From:{user_email}
+{raw_message}
+"""
+    button = st.form_submit_button("submit")
+
+    if button:
+        send_email(message)
+        st.info("email send successfully ")
---
+++ send_email.py
@@ -0,0 +1,17 @@
+import smtplib
+import ssl
+
+
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
+    username = "beerankutty1509@gmail.com"
+    pasword = "sbbvdfarbygnwcwg"
+
+    context = ssl.create_default_context()
+    receiver = "beerankutty1509@gmail.com"
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, pasword)
+        server.sendmail(username, receiver, message)
+
+
